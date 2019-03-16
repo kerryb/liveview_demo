08591b2ffda1ec99103106306ce0f8e96c29764d
Kerry Buckley <kerryjbuckley@gmail.com>
2019-03-16 18:46:20 +0000

    Create dummy page

--- lib/liveview_demo_web/views/page_view.ex
+++ lib/liveview_demo_web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule LiveviewDemoWeb.PageView do
   use LiveviewDemoWeb, :view
+
+  def status_class(n) when n < 50, do: "ok"
+  def status_class(n) when n < 90, do: "warning"
+  def status_class(n), do: "danger"
 end
