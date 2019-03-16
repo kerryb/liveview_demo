a121af20f9a71c98308154ecc7695acb29ab7e89
Kerry Buckley <kerryjbuckley@gmail.com>
2019-03-16 21:08:38 +0000

    Store counters in server state

--- lib/liveview_demo/counters.ex
+++ lib/liveview_demo/counters.ex
@@ -2,15 +2,20 @@ defmodule LiveviewDemo.Counters do
   use GenServer
 
   def start_link(number_of_counters) do
-    GenServer.start_link(__MODULE__, number_of_counters)
+    GenServer.start_link(__MODULE__, number_of_counters, name: __MODULE__)
   end
 
   @impl true
   def init(number_of_counters) do
-    {:ok, 1..100 |> Enum.shuffle()}
+    {:ok, 1..number_of_counters |> Enum.shuffle()}
   end
 
   def all do
-    1..100 |> Enum.shuffle()
+    GenServer.call(__MODULE__, :all)
+  end
+
+  @impl true
+  def handle_call(:all, _from, counters) do
+    {:reply, counters, counters}
   end
 end
